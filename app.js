a8e417d62d7c03056b6eff6f52c0bf8c91e68cd3
beki-jone <bekojone@gmail.com>
2024-03-16 19:56:47 +0300

    f

--- app.js
+++ app.js
@@ -2,28 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
 const app = express();
+
 // Enable CORS for all routes
 app.use(cors());
+
 const port = process.env.PORT || 4000;
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
 const dbConnection = require("./db/dbConfig");
 const userRoutes = require("./routes/userRoute");
 const questionRoutes = require("./routes/questionRoute");
 const answerRoutes = require("./routes/answerRoute");
+
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/questions", questionRoutes);
 app.use("/api/answers", answerRoutes);
+
 async function start() {
   try {
     const result = await dbConnection.execute("select 'test' ");
-     app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
   } catch (error) {
     console.log(error.message);
   }
 }
+
 start();
